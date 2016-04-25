8d7149e957678338d9466f35fc4a94b5df70da2f
Erik Gomez <egoxiv@gmail.com>
2016-04-25 16:03:55 -0700

    null

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -5,7 +5,12 @@ var userRoutes		= express.Router();
 //create routes for /users
 userRoutes.get('/', function(req, res) {
   var destinations = ['Burger Lounge', 'True Food', 'Tender Greens', '800 Degree Pizza'];
-  res.render('home/index', {message: destinations});
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
+  newdate = month + "/" + day + "/" + year;
+  res.render('home/index', {message: destinations, todayIs: newdate});
 });
 
 
